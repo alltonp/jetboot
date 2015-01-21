90400dcd3f2f58e01dad158bffcddbd37eb1ab68
alltonp <paulallton@gmail.com>
2015-01-21 09:39:06 +0000

    first blood part III

---
+++ src/main/scala/im/mange/jetboot/Css.scala
@@ -0,0 +1,5 @@
+package im.mange.jetboot
+
+import im.mange.jetboot.css.{StyleValues, StyleKeys}
+
+object Css extends StyleKeys with StyleValues
---
+++ src/main/scala/im/mange/jetboot/bootstrap3/Bootstrap.scala
@@ -0,0 +1,57 @@
+package im.mange.jetboot.bootstrap3
+
+object Bootstrap {
+  val alert = "alert"
+  val alertDanger = "alert-danger"
+
+  val panel = "panel"
+  val panelBody = "panel-body"
+  val panelDefault = "panel-default"
+  val panelFooter = "panel-footer"
+  val panelHeading = "panel-heading"
+
+  val panelPrimary = "panel-primary"
+  val panelSuccess ="panel-success"
+  val panelInfo ="panel-info"
+  val panelWarning ="panel-warning"
+  val panelDanger ="panel-danger"
+
+  val pullLeft = "pull-left"
+  val pullRight = "pull-right"
+
+  val tableBordered = "table-bordered"
+  val tableCondensed = "table-condensed"
+  val tableStriped = "table-striped"
+
+  val textCenter = "text-center"
+  val textLeft = "text-left"
+  val textRight = "text-right"
+  val textJustify = "text-justify"
+
+  val container = "container"
+  val row = "row"
+  val colLg6 = "col-lg-6"
+  val colLg12 = "col-lg-12"
+
+  //TODO: should probably be pulled out
+  val glyphicon = "glyphicon"
+  def glyphicon(name: String) = "glyphicon glyphicon-" + name
+
+  val formControl = "form-control"
+  val inputGroupAddon = "input-group-addon"
+  val inputGroup = "input-group"
+  val inputGroupSm = "input-group-sm"
+  val inputGroupBtn = "input-group-btn"
+
+  val label = "label"
+  val labelDefault = "label-default"
+  val labelPrimary = "label-primary"
+  val labelSuccess = "label-success"
+  val labelInfo = "label-info"
+  val labelWarning = "label-warning"
+  val labelDanger = "label-danger"
+
+  //TODO:
+  //btn
+  //btn-default
+}
