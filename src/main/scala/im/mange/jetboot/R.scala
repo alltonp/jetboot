c91d4dd84659faa4bdd03394913ec0f1cbd06188
alltonp <paul.allton@theorem.technology>
2015-02-18 17:43:35 +0000

    make it possible to create R with a list

--- src/main/scala/im/mange/jetboot/R.scala
+++ src/main/scala/im/mange/jetboot/R.scala
@@ -4,6 +4,7 @@ import scala.xml.{NodeSeq, Text}
 
 object R {
   def apply(renderables: Renderable*): Renderable = R(Composite(renderables:_*).render)
+  def apply(renderables: List[Renderable]): Renderable = R(renderables:_*)
   def apply(content: String): Renderable = R(Text(content))
   def apply(): Renderable = R(NodeSeq.Empty)
 }
