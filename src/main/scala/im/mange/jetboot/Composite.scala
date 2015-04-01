7a844d2335d12c6941bbd67bb1d120edf0d4b947
alltonp <paul.allton@theorem.technology>
2015-04-01 10:13:23 +0100

    support multiple NodeSeq in R()

--- src/main/scala/im/mange/jetboot/Composite.scala
+++ src/main/scala/im/mange/jetboot/Composite.scala
@@ -4,6 +4,7 @@ import scala.xml.NodeSeq
 
 //TODO: we do this a lot - consider C()
 //TODO: allow creation from a Seq too .. otherwise we end up doing a lot of :_*
+//TODO: do we still need this? Has R() replaced it?
 case class Composite(renderables: Renderable*) extends Renderable {
   def render = renderables.foldLeft(NodeSeq.Empty) {(result, r) ⇒ result ++ r.render }
 }
--- src/main/scala/im/mange/jetboot/R.scala
+++ src/main/scala/im/mange/jetboot/R.scala
@@ -3,12 +3,12 @@ package im.mange.jetboot
 import scala.xml.{NodeSeq, Text}
 
 object R {
-  def apply(renderables: Renderable*): Renderable = R(Composite(renderables:_*).render)
+  def apply(renderables: Renderable*): Renderable = R(renderables.map(_.render):_*)
   def apply(renderables: List[Renderable]): Renderable = R(renderables:_*)
   def apply(content: String*): Renderable = R(content.map(Text(_)))
   def apply(): Renderable = R(NodeSeq.Empty)
 }
 
-case class R(private val content: NodeSeq) extends Renderable {
-  def render = content
+case class R(private val content: NodeSeq*) extends Renderable {
+  def render = content.foldLeft(NodeSeq.Empty) {(result, r) ⇒ result ++ r }
 }
