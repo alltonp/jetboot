7ed314f5a3a25c0e0219164b9fb7b467abdada23
alltonp <paul.allton@theorem.technology>
2015-05-01 11:52:49 +0100

    further deprecations

--- src/main/scala/im/mange/jetboot/Input.scala
+++ src/main/scala/im/mange/jetboot/Input.scala
@@ -4,4 +4,5 @@ package im.mange.jetboot
 //TODO: should this be Hideable? - nah that should be on Widget
 //TODO: should this be Styleable? - think no actually because of ButtonPresentation .. but that could be bogus
 //TOD: should we have an Updatable? - with element.setValue(value)
+@deprecated("I need to die", "01/05/2015")
 trait Input extends Widget with Disableable with Focusable
--- src/main/scala/im/mange/jetboot/Widget.scala
+++ src/main/scala/im/mange/jetboot/Widget.scala
@@ -4,4 +4,5 @@ package im.mange.jetboot
 //TODO: should I be Hideable ? almost definitely
 //TODO: or should probably keep this trait to the minimum and mixin Hideable, Disableable etc - nah have a lower level trait for that
 //TODO: possibly this could die now .. HtmlElement should not implement it ...
+@deprecated("I need to die", "01/05/2015")
 trait Widget extends Identifiable with Renderable
