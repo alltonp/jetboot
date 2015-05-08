85adfa4c0aefbc31e9c5ad03ebdc0748f5970794
alltonp <paul.allton@theorem.technology>
2015-05-08 11:49:18 +0100

    make Span be a Fillable

--- src/main/scala/im/mange/jetboot/html/Span.scala
+++ src/main/scala/im/mange/jetboot/html/Span.scala
@@ -1,15 +1,11 @@
 package im.mange.jetboot.html
 
-import im.mange.jetboot.{HtmlElement, Styleable, Hideable, Renderable}
+import im.mange.jetboot._
 
 //TODO: I should have minimum show/hide, fill/empty
 //TODO: I dont think I should be a case class actually - maybe be trait or abstract class with an Object
 //TODO: if we stopped this being a Widget and Hideable, then id would no longer need to be mandatory
 //TODO: show be fillable like a div?
-case class Span(id: String, content: Renderable) extends /*Widget with*/ Hideable with Styleable with HtmlElement {
+case class Span(id: String, content: Renderable) extends /*Widget with*/ Hideable with Fillable with Styleable with HtmlElement {
   def render = <span id={id} class={classes.render} style={styles.render} title={title}>{content.render}</span>
-
-  //TODO: these should be on fillable I think
-  def fill(content: Renderable) = element.fill(content)
-  def empty = element.empty
 }
