28bcaa27ece7f8accbaa1f29ad7bc174d39c2255
alltonp <paul.allton@theorem.technology>
2015-05-08 11:52:18 +0100

    add some Hmmmm's

--- src/main/scala/im/mange/jetboot/html/Div.scala
+++ src/main/scala/im/mange/jetboot/html/Div.scala
@@ -2,14 +2,14 @@ package im.mange.jetboot.html
 
 import im.mange.jetboot._
 
-//TODO: I should have minimum show/hide, fill/empty
 //TODO: for fill() to work we need a proper div, need to think about that a bit
 //TODO: I dont think I should be a case class actually - maybe be trait or abstract class with an Object
 //TODO: er ... arent these Elements actually ... and Widgets are more highlevel - yes I should be in jetboot.html.element or something
 //TODO: SimpleWidgets could mixin all the Bootstrap style, StyleKeys and StyleVals stuff for ease of typing/conversion (StyleFactory)
 //TODO: maybe have a LayoutFactory to match
-//TODO: possibly WidgetFactory is ElementFactory actually
-case class Div(id: String, content: Renderable) extends /*Widget with*/ Hideable with Fillable with Styleable with HtmlElement {
+
+//Hmm ... id should be an Option ... things that depend on id e.g. Fillable etc should do Js.nothing on a None
+case class Div(id: String, content: Renderable) extends Hideable with Fillable with Styleable with HtmlElement {
   def render = <div id={id} class={classes.render} style={styles.render} title={title}>{content.render}</div>
 }
 
--- src/main/scala/im/mange/jetboot/html/Span.scala
+++ src/main/scala/im/mange/jetboot/html/Span.scala
@@ -2,10 +2,10 @@ package im.mange.jetboot.html
 
 import im.mange.jetboot._
 
-//TODO: I should have minimum show/hide, fill/empty
 //TODO: I dont think I should be a case class actually - maybe be trait or abstract class with an Object
 //TODO: if we stopped this being a Widget and Hideable, then id would no longer need to be mandatory
-//TODO: show be fillable like a div?
-case class Span(id: String, content: Renderable) extends /*Widget with*/ Hideable with Fillable with Styleable with HtmlElement {
+
+//Hmm ... id should be an Option ... things that depend on id e.g. Fillable etc should do Js.nothing on a None
+case class Span(id: String, content: Renderable) extends Hideable with Fillable with Styleable with HtmlElement {
   def render = <span id={id} class={classes.render} style={styles.render} title={title}>{content.render}</span>
 }
