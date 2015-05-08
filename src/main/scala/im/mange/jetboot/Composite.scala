bba738461a3a17e08c777f5ab27b6c33a3b5afbc
alltonp <paul.allton@theorem.technology>
2015-05-08 12:06:06 +0100

    deprecate Composite

--- src/main/scala/im/mange/jetboot/Composite.scala
+++ src/main/scala/im/mange/jetboot/Composite.scala
@@ -2,9 +2,8 @@ package im.mange.jetboot
 
 import scala.xml.NodeSeq
 
-//TODO: we do this a lot - consider C()
 //TODO: allow creation from a Seq too .. otherwise we end up doing a lot of :_*
-//TODO: do we still need this? Has R() replaced it?
+@deprecated("Use R() instead", "08/05/2015")
 case class Composite(renderables: Renderable*) extends Renderable {
   def render = renderables.foldLeft(NodeSeq.Empty) {(result, r) ⇒ result ++ r.render }
 }
