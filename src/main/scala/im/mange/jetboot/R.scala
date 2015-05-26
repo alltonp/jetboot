90caafa6345b4874e0b030b06a39121eaad6dc45
alltonp <paul.allton@theorem.technology>
2015-05-26 16:50:15 +0100

    a little less List ...

--- src/main/scala/im/mange/jetboot/R.scala
+++ src/main/scala/im/mange/jetboot/R.scala
@@ -6,9 +6,9 @@ object R {
   //TIP: http://spray.io/blog/2012-12-13-the-magnet-pattern/
   sealed trait RenderableMagnet { def apply(): Renderable }
   implicit def fromString(s: String) = new RenderableMagnet { def apply() = R(Text(s)) }
-  implicit def fromStrings(ss: List[String]) = new RenderableMagnet { def apply() = R(Text(ss.mkString)) }
+  implicit def fromStrings(ss: Seq[String]) = new RenderableMagnet { def apply() = R(Text(ss.mkString)) }
   implicit def fromRenderable(r: Renderable) = new RenderableMagnet { def apply() = r }
-  implicit def fromRenderables(r: List[Renderable]) = new RenderableMagnet { def apply() = Composite(r:_*) }
+  implicit def fromRenderables(r: Seq[Renderable]) = new RenderableMagnet { def apply() = Composite(r:_*) }
   implicit def fromNodeSeq(n: NodeSeq) = new RenderableMagnet { def apply() = R(n) }
 
   def apply(): Renderable = R(NodeSeq.Empty)
