fa5b86fbeb0746263c2bf2915e6918012405530f
alltonp <paul.allton@theorem.technology>
2015-05-26 16:53:36 +0100

    remove spaces from ids

--- src/main/scala/im/mange/jetboot/input/Field.scala
+++ src/main/scala/im/mange/jetboot/input/Field.scala
@@ -3,7 +3,7 @@ package im.mange.jetboot.input
 import im.mange.little.string.Strings
 
 case class Field(name: String, var parent: Option[String] = None) {
-  private val safeId = name.replaceAll("\\.", "")
+  private val safeId = name.replaceAll("\\.", "").replaceAll(" ", "")
   //TODO: makeSafe, i.e. remove other funny id breaking chars etc
   //see http://stackoverflow.com/questions/70579/what-are-valid-values-for-the-id-attribute-in-html
   //.replaceAll(" ", "").replaceAll(".", "").replaceAll(":", "")
