75d15e55a466965fade58c20b3a142ce914e9f37
alltonp <paulallton@gmail.com>
2015-07-21 11:10:02 +0100

    combine the client side and server side anchors

--- src/main/scala/im/mange/jetboot/html/A.scala
+++ src/main/scala/im/mange/jetboot/html/A.scala
@@ -4,12 +4,11 @@ import im.mange.jetboot.{Styleable, Renderable}
 import net.liftweb.http.SHtml._
 import net.liftweb.http.js.JsCmd
 
-case class ClientSideAnchor(id: String, content: Renderable, onClick: JsCmd) extends Renderable with Styleable {
-  def render = a(content.render, onClick, "id" → id, "style" → styles.render, "class" → classes.render)
-}
-
-case class ServerSideAnchor(id: String, content: Renderable, onClick: JsCmd) extends Renderable with Styleable {
-  def render = a(() ⇒ onClick, content.render, "id" → id, "style" → styles.render, "class" → classes.render)
+case class A(id: String, content: Renderable, onClick: JsCmd, ajax: Boolean) extends Renderable with Styleable {
+  def render = {
+    if (ajax) a(() ⇒ onClick, content.render, "id" → id, "style" → styles.render, "class" → classes.render)
+    else a(content.render, onClick, "id" → id, "style" → styles.render, "class" → classes.render)
+  }
 }
 
 case class LinkAnchor(id: String, url: String, content: Renderable, target: Option[String]) extends Renderable with Styleable {
