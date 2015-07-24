66b837683f5382752890c31ea567aa99270564bb
alltonp <paul.allton@theorem.technology>
2015-07-24 17:17:12 +0100

    twiddle a string

--- src/main/scala/im/mange/jetboot/page/StaticPage.scala
+++ src/main/scala/im/mange/jetboot/page/StaticPage.scala
@@ -3,5 +3,5 @@ package im.mange.jetboot.page
 import scala.xml.NodeSeq
 
 trait StaticPage extends Page {
-  override def decorate(content: NodeSeq) = <div class="lift:surround?with=thing;at=content">{ content }</div>
+  override def decorate(content: NodeSeq) = <div class="lift:surround?with=app;at=content">{ content }</div>
 }
