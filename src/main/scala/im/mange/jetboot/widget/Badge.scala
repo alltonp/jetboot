ccf79d720e65f35346f5e3b60ba3186aedf852af
alltonp <paul.allton@theorem.technology>
2015-08-05 17:10:32 +0100

    add badge

---
+++ src/main/scala/im/mange/jetboot/widget/Badge.scala
@@ -0,0 +1,13 @@
+package im.mange.jetboot.widget
+
+import im.mange.jetboot.{Html, Identifiable, R, Renderable}
+
+case class Badge(id: String, value: String) extends Renderable with Identifiable {
+  import Html._
+
+  private val content = span(Some(id), R(value)).classes("badge", if (value.trim.isEmpty) "hidden" else "")
+
+  override def render = content.render
+
+  def update(value: String) = if (value.trim().isEmpty) content.emptyAndHide else content.fillAndShow(R(value))
+}
