d6aaa5312aa7f48458e70553cc3bfc7d00ff4520
pall <paulallton@gmail.com>
2015-08-11 13:44:06 +0100

    doh .. whack Composite

--- src/main/scala/im/mange/jetboot/Composite.scala
+++
@@ -1,9 +0,0 @@
-package im.mange.jetboot
-
-import scala.xml.NodeSeq
-
-//TODO: allow creation from a Seq too .. otherwise we end up doing a lot of :_*
-@deprecated("Use R() instead", "08/05/2015")
-case class Composite(renderables: Renderable*) extends Renderable {
-  def render = renderables.foldLeft(NodeSeq.Empty) {(result, r) ⇒ result ++ r.render }
-}
