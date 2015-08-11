fdb91c32962b5a6b86f8f07c29a0dcbc069bc1e5
pall <paulallton@gmail.com>
2015-08-11 14:32:15 +0100

    make Hx take Renderables

--- src/main/scala/im/mange/jetboot/bootstrap3/Typography.scala
+++ src/main/scala/im/mange/jetboot/bootstrap3/Typography.scala
@@ -1,12 +1,13 @@
 package im.mange.jetboot.bootstrap3
 
+import im.mange.jetboot.Renderable
 import im.mange.jetboot.bootstrap3.typography.Hx
 
 object Typography {
-  def h1(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 1)
-  def h2(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 2)
-  def h3(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 3)
-  def h4(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 4)
-  def h5(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 5)
-  def h6(heading: String, secondary: Option[String] = None) = Hx(heading, secondary, 6)
+  def h1(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 1)
+  def h2(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 2)
+  def h3(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 3)
+  def h4(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 4)
+  def h5(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 5)
+  def h6(heading: Renderable, secondary: Option[Renderable] = None) = Hx(heading, secondary, 6)
 }
--- src/main/scala/im/mange/jetboot/bootstrap3/typography/Hx.scala
+++ src/main/scala/im/mange/jetboot/bootstrap3/typography/Hx.scala
@@ -4,6 +4,6 @@ import im.mange.jetboot.Css._
 import im.mange.jetboot.Html._
 import im.mange.jetboot.{R, Renderable}
 
-case class Hx(heading: String, secondary: Option[String], x: Int) extends Renderable {
-  def render = span(span(R(heading)), secondary.fold(span())(s => span(R(s" ${s}"))).classes(small)).classes(s"h$x").render
+case class Hx(heading: Renderable, secondary: Option[Renderable], x: Int) extends Renderable {
+  def render = span(span(heading), secondary.fold(span())(s => span(R(" "), s)).classes(small)).classes(s"h$x").render
 }
