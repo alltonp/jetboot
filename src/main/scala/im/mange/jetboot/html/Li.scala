13f222dda6e264f7ed6fbbddad377179e8f073f7
alltonp <paul.allton@theorem.technology>
2015-08-12 15:08:59 +0100

    make Li be HasAttributes

--- src/main/scala/im/mange/jetboot/html/Li.scala
+++ src/main/scala/im/mange/jetboot/html/Li.scala
@@ -1,7 +1,15 @@
 package im.mange.jetboot.html
 
-import im.mange.jetboot.{Styleable, Renderable}
+import im.mange.jetboot.{HasAttributes, Styleable, Renderable}
+import net.liftweb.http.S._
+import net.liftweb.http.SHtml.ElemAttr
+import net.liftweb.http.js.JE.Str
 
-case class Li(content: Renderable) extends Renderable with Styleable {
-  def render = <li class={classes.render} style={styles.render}>{content.render}</li>
+case class Li(content: Renderable) extends Renderable with Styleable with HasAttributes {
+  def render = {
+    val allAttributes: Seq[(String, String)] = Map("style" → styles.render, "class" → classes.render).toSeq ++ attributes.toSeq
+    val elemAttrs = ElemAttr.strSeqToElemAttr(allAttributes)
+    val base = <li class={classes.render} style={styles.render}>{content.render}</li>
+    elemAttrs.foldLeft(base)(_ % _)
+  }
 }
