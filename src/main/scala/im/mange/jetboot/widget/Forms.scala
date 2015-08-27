1ac06ce0e78457280537bbadadf75c7eef8a83e1
alltonp <paulallton@gmail.com>
2015-08-27 22:51:41 +0100

    a little renaming

--- src/main/scala/im/mange/jetboot/widget/Forms.scala
+++ src/main/scala/im/mange/jetboot/widget/Forms.scala
@@ -7,7 +7,8 @@ import im.mange.jetpac.Renderable
 trait Forms extends FormLayouts {
   import im.mange.jetboot.widget.form._
 
-  def simpleForm(id: String, inputs: Seq[FormInput]) = Form(id, inputs)
+  def form(id: String, inputs: Seq[FormInput]) = Form(id, inputs)
+
   def formHolder(columns: Int, formLayout: FormLayout, beforeLayout: Option[Renderable] = None,
                  afterLayout: Option[Renderable] = None) = FormHolder(columns, formLayout, beforeLayout, afterLayout)
 }
