2306cfa7b2cbda10e2d2c08e4968dd03f41c5fa2
alltonp <paul.allton@theorem.technology>
2015-08-28 17:48:03 +0100

    more todoing

--- src/main/scala/im/mange/jetboot/widget/ButtonPresentation.scala
+++ src/main/scala/im/mange/jetboot/widget/ButtonPresentation.scala
@@ -6,5 +6,5 @@ import scala.xml.NodeSeq
 
 //TODO: possibly this should be a trait with implementations
 //TODO: hmmm .. not sure this looks so good these days
-@deprecated("Use HtmlInput instead", "01/05/2015")
+@deprecated("Use new ButtonPresentation instead", "28/08/2015")
 case class ButtonPresentation(body: NodeSeq, classes: Classes = Classes(), styles: Styles = Styles())
