4e8d2a967adf72f94ff04b32a754dece5e4e989a
alltonp <paulallton@gmail.com>
2015-09-01 15:07:52 +0100

    and finally ...

--- src/main/scala/im/mange/jetpac/input/SubmitButton.scala
+++ src/main/scala/im/mange/jetpac/input/SubmitButton.scala
@@ -7,26 +7,7 @@ import net.liftweb.http.js.JsCmd
 
 //TODO: Styleable must die here ...
 case class SubmitButton(id: String, buttonPresentation: ButtonPresentation, onSubmit: () => JsCmd) extends Input with Styleable {
-
-//  def baseElement = SHtml.text(value, onSubmit, "id" → id, "style" → styles.render, "class" → classes.render,
-//    "placeholder" → placeholder.getOrElse(""))
-
-//  private def onSubmit(value: String) { this.value = value }
-
-//  def onChange(handler: String => JsCmd): this.type = addEvents(Event.onChange -> handler)
-//  def onKeyUp(handler: String => JsCmd): this.type = addEvents(Event.onKeyUp -> handler)
-
-
-//  def render = ajaxSubmit(value, onSubmit, "id" -> id, "style" -> styles.render, "class" -> classes.render)
-//  def render = ajaxOnSubmit(onSubmit)(<input id={id} type="submit" style={buttonPresentation.styles.add(styles.styles:_*).render}
-//                      class={buttonPresentation.classes.add(classes.classNames:_*).render}
-//                      ></input>)
-
-  def render = ajaxOnSubmit(onSubmit)(<button id={id} style={buttonPresentation.styles.add(styles.styles:_*).render}
-                      class={buttonPresentation.classes.add(classes.classNames:_*).render}
-                      >{buttonPresentation.renderable}</button>)
-
-//  def render = <input id={id} type="submit" style={buttonPresentation.styles.add(styles.styles:_*).render}
-//                      class={buttonPresentation.classes.add(classes.classNames:_*).render}
-//                      onclick={ajaxOnSubmit(onSubmit)}></input>
+  def render = ajaxOnSubmit(onSubmit)(<button id={id}
+              style={buttonPresentation.styles.add(styles.styles:_*).render}
+              class={buttonPresentation.classes.add(classes.classNames:_*).render}>{buttonPresentation.renderable.render}</button>)
 }
