722710f505824c434558739c7f08e0d0008ba62c
alltonp <paul.allton@theorem.technology>
2015-09-02 14:16:33 +0100

    really add stackable

--- src/main/scala/im/mange/jetboot/Fa.scala
+++ src/main/scala/im/mange/jetboot/Fa.scala
@@ -1,14 +1,14 @@
 package im.mange.jetboot
 
-import im.mange.jetboot.widget.fontawesome.Fai
+import im.mange.jetboot.widget.fontawesome.{Fas, Fai}
 
 object Fa extends FaElements
 
 trait FaElements {
   //TIP: see http://fortawesome.github.io/Font-Awesome/cheatsheet/
 
-  def fa(icon: String, size: Int = 0, fixedWidth: Boolean = false, spin: Boolean = false, pulse: Boolean = false) =
-    Fai(icon, size, fixedWidth, spin, pulse)
+  def fa(icon: String, size: Int = 0, fixedWidth: Boolean = false, spin: Boolean = false, pulse: Boolean = false, inverse: Boolean = false) =
+    Fai(icon, size, fixedWidth, spin, pulse, inverse)
 
   def fas(icons: Seq[Fai], size: Int = 0) = Fas(icons, size)
 }
