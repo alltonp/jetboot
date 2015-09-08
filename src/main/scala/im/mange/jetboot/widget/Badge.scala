0ae3fc113cf4f8308366937cc3f25d8950dc6582
alltonp <paul.allton@theorem.technology>
2015-09-08 17:28:00 +0100

    sonatype often sucks

--- src/main/scala/im/mange/jetboot/widget/Badge.scala
+++ src/main/scala/im/mange/jetboot/widget/Badge.scala
@@ -2,6 +2,9 @@ package im.mange.jetboot.widget
 
 import im.mange.jetpac._
 
+//TIP: it is strongly recommended to set 0 values to be "" because
+//(1) it looks better - given that badges are largely for todos
+//(2) its must easier to test in phantom (where easy = possible)
 case class Badge(id: String, value: String) extends Renderable with Identifiable with Styleable {
   import Html._
 
