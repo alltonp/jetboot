e24f7bcd6e7701cc8a7a12c7d85e48b5a0f0e4a9
alltonp <paul.allton@theorem.technology>
2015-09-08 17:49:48 +0100

    hmm .. make Badge non-styleable but support bending in extra classes

--- src/main/scala/im/mange/jetboot/widget/Badge.scala
+++ src/main/scala/im/mange/jetboot/widget/Badge.scala
@@ -1,16 +1,16 @@
 package im.mange.jetboot.widget
 
 import im.mange.jetpac._
+import im.mange.jetpac.css.Classes
 
 //TIP: it is strongly recommended to set 0 values to be "" because
 //(1) it looks better - given that badges are largely for todos
 //(2) its must easier to test in phantom (where easy = possible)
-case class Badge(id: String, value: String) extends Renderable with Identifiable with Styleable {
+case class Badge(id: String, value: String, buttonClasses: Classes = Classes) extends Renderable with Identifiable {
   import Html._
 
   private val content = span(Some(id), R(value))
-    .styles(styles)
-    .classes(classes.add("badge", if (value.trim.isEmpty) "hidden" else ""))
+    .classes(buttonClasses.add("badge", if (value.trim.isEmpty) "hidden" else ""))
 
   override def render = content.render
 
