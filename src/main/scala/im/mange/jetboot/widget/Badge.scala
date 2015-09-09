e43043a4e4c914bf0e5ec513f93bf0181fc9f812
alltonp <paul.allton@theorem.technology>
2015-09-09 11:04:39 +0100

    add styles to the Badge

--- src/main/scala/im/mange/jetboot/widget/Badge.scala
+++ src/main/scala/im/mange/jetboot/widget/Badge.scala
@@ -1,16 +1,17 @@
 package im.mange.jetboot.widget
 
 import im.mange.jetpac._
-import im.mange.jetpac.css.Classes
+import im.mange.jetpac.css.{Styles, Classes}
 
 //TIP: it is strongly recommended to set 0 values to be "" because
 //(1) it looks better - given that badges are largely for todos
 //(2) its must easier to test in phantom (where easy = possible)
-case class Badge(id: String, value: String, buttonClasses: Classes = Classes()) extends Renderable with Identifiable {
+case class Badge(id: String, value: String, classes: Classes = Classes(), styles: Styles = Styles()) extends Renderable with Identifiable {
   import Html._
 
   private val content = span(Some(id), R(value))
-    .classes(buttonClasses.add("badge", if (value.trim.isEmpty) "hidden" else ""))
+    .classes(classes.add("badge", if (value.trim.isEmpty) "hidden" else ""))
+    .styles(styles)
 
   override def render = content.render
 
