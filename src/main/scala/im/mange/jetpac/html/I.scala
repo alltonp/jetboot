dd1198cd866d5cf42ca4563f07b6a4a56e311769
alltonp <paulallton@gmail.com>
2015-10-07 15:04:50 +0100

    centralise the Attributification

--- src/main/scala/im/mange/jetpac/html/I.scala
+++ src/main/scala/im/mange/jetpac/html/I.scala
@@ -1,15 +1,25 @@
 package im.mange.jetpac.html
 
 import im.mange.jetpac.{HasAttributes, Renderable, Styleable}
+import net.liftweb.http.SHtml
 import net.liftweb.http.SHtml.ElemAttr
+
+import scala.xml.Elem
 
 //TIP: this is the model for all future Elements ...
 //TODO: make base be the smallest possible .. i.e remove class and style
 case class I(content: Renderable) extends Renderable with Styleable with HasAttributes {
   def render = {
     val allAttributes: Seq[(String, String)] = Map("style" → styles.render, "class" → classes.render).toSeq ++ attributes.toSeq
-    val elemAttrs = ElemAttr.strSeqToElemAttr(allAttributes)
-    val base = <i>{content.render}</i>
-    elemAttrs.foldLeft(base)(_ % _)
+    Attributify(<i>{content.render}</i>, allAttributes)
+  }
+}
+
+object Attributify {
+  import net.liftweb.http.SHtml.ElemAttr
+
+  def apply(elem: Elem, allAttributes: Seq[(String, String)]) = {
+    val elemAttrs: Seq[SHtml.ElemAttr] = ElemAttr.strSeqToElemAttr(allAttributes)
+    elemAttrs.foldLeft(elem)(_ % _)
   }
 }
