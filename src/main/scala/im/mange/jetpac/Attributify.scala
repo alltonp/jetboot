a2474735d8c54dd3644f9ab778c78dfe63f81650
alltonp <paulallton@gmail.com>
2015-10-07 15:27:46 +0100

    filter out empty values in with HasAttributes

--- src/main/scala/im/mange/jetpac/Attributify.scala
+++ src/main/scala/im/mange/jetpac/Attributify.scala
@@ -8,7 +8,9 @@ object Attributify {
   import net.liftweb.http.SHtml.ElemAttr
 
   def apply(elem: Elem, allAttributes: Seq[(String, String)]) = {
-    val elemAttrs: Seq[SHtml.ElemAttr] = ElemAttr.strSeqToElemAttr(allAttributes)
+    val elemAttrs: Seq[SHtml.ElemAttr] = ElemAttr.strSeqToElemAttr(cleanse(allAttributes))
     elemAttrs.foldLeft(elem)(_ % _)
   }
+
+  private def cleanse(attributes: Seq[(String, String)]) = attributes.filterNot(a => a._2.isEmpty)
 }
